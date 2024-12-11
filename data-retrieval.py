57c1e25be5ab02202a32ba4ed7dde1949e35d331
Benedict O. Ejelonu <59194091+Bwhiz@users.noreply.github.com>
2024-12-11 18:10:50 +0000

    translating from pymc to Numpyro

---
+++ data-retrieval.py
@@ -0,0 +1,7 @@
+# using pymc to retrieve the data
+import pymc as pm
+import pandas as pd
+
+data = pd.read_csv(pm.get_data("anemones.csv"))
+data.to_csv("data/anemones.csv",index=False)
+print("=== saved csv data ===")
